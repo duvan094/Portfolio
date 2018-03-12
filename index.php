c23dcdb3894300615bb0e867460b0c6d39423585
Jacob <jacobduvander@gmail.com>
2018-03-12 23:28:10 +0100

    Some basic structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jacob Duvander</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Under uppbyggnad.</h1>
+  </body>
+</html>
