79c17776901d690c6ffde56731b414e3d172d01c
Jacob <jacobduvander@gmail.com>
2018-03-17 18:46:56 +0100

    Added next/prev

--- singular.php
+++ singular.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="hero" style="background:url(<?php echo get_the_post_thumbnail_url($post->ID,'full')?>); background-size:cover; background-position:center; background-attachment:fixed;">
+<div class="hero" style="background:url(<?php echo get_the_post_thumbnail_url($post->ID,'full')?>); background-size:cover; background-position:center;">
 
 </div>
 
@@ -13,10 +13,13 @@
         echo "<h1>" . get_the_title() . "</h1>";
         the_content();
         echo do_shortcode(get_post_meta($post->ID,"project_gallery",true));
+        echo "<div class='next-prev-post'>";
+        next_post_link( '%link' );
+        previous_post_link( '%link' );
+        echo "</div>";
       }
     }
     ?>
-
   </div>
 </article>
 
