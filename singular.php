0708b0cd5b5cf059c12669b7272b6b35ab533e9b
Jacob <jacobduvander@gmail.com>
2018-03-18 18:17:19 +0100

    Some css changes

--- singular.php
+++ singular.php
@@ -1,27 +1,37 @@
 <?php get_header(); ?>
 
-<div class="hero" style="background:url(<?php echo get_the_post_thumbnail_url($post->ID,'full')?>); background-size:cover; background-position:center;">
+<?php
+if(have_posts()){
+  while(have_posts()){
+    the_post();
 
+?>
+<div class="hero-container">
+  <div class="hero project" style="background:url(<?php echo get_the_post_thumbnail_url($post->ID,'full')?>); background-size:cover; background-position:center;">
+  </div>
+  <?php echo "<h1>" . get_the_title() . "</h1>"; ?>
 </div>
+
 
 <article>
   <div class="wrapper project">
     <?php
-    if(have_posts()){
-      while(have_posts()){
-        the_post();
-        echo "<h1>" . get_the_title() . "</h1>";
-        the_content();
         echo do_shortcode(get_post_meta($post->ID,"project_gallery",true));
+        the_content();
         echo "<div class='next-prev-post'>";
         next_post_link( '%link' );
         previous_post_link( '%link' );
         echo "</div>";
-      }
-    }
     ?>
   </div>
 </article>
+
+<?php
+  }
+}//the loop end
+
+ ?>
+
 
 <script type="text/javascript">
 
