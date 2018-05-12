42eadad74de2674f4a3eda677060048bb98c167e
Jacob <jacobduvander@gmail.com>
2018-05-12 16:05:31 +0200

    Added scroll animation

--- front-page.php
+++ front-page.php
@@ -15,9 +15,9 @@
       </div>
     </div>
     <!--<h2>Graphic designer & Web developer</h2>-->
-  <img id="arrow" src="<?php bloginfo('template_url'); ?>/img/arrow.svg" alt="arrow">
+  <img id="arrow" src="<?php bloginfo('template_url'); ?>/img/arrow-white.svg" alt="arrow">
 </div>
-<div class="featured-title">
+<div class="featured-title" id="scroll-finish">
   <h3>Latest Projects</h3>
 </div>
 <?php
@@ -48,7 +48,7 @@ if ($my_query->have_posts()):
 endif;
 
 ?>
-<article class="about-me invisible">
+<article class="about-me invisible" id="about">
   <div class="wrapper">
     <?php dynamic_sidebar('about-me'); ?>
     <?php dynamic_sidebar('profile-img'); ?>
@@ -58,9 +58,60 @@ endif;
 <script type="text/javascript">
 
   document.getElementById("arrow").addEventListener("click",function(){
-      console.log("Arrow click");
-      document.getElementById("hero").classList.toggle("minimize");
+    event.preventDefault();
+    scroll("scroll-finish");
   });
+
+
+  /*
+    Add scroll animation to about-me which is on the same page.
+  */
+  var anchor;
+  var elements = document.getElementsByTagName('a');
+  for(var i = 0; i < elements.length; i++) {
+    if(elements[i].href.includes("#")){
+      var linkArr = elements[i].href.split("#");
+      anchor = linkArr[linkArr.length-1];
+      elements[i].addEventListener("click",function(){
+        event.preventDefault();
+        scroll(anchor);
+      })
+    }
+  }
+
+
+  var scrollFinish;
+  var scrollPos;
+  var maxSpeed = 40;
+  var easeSpeed = 2;
+
+  var easeLength = 0;
+  for(var i = 0; i<=maxSpeed; i++){//Calculate the when the easeEnd is to begin
+    easeLength += maxSpeed - (i*easeSpeed);
+  }
+
+  function scroll(id){
+    scrollFinish = document.getElementById(id).getBoundingClientRect().top - 110 + window.scrollY;
+    scrollPos = window.scrollY;
+    scrollAnimate();
+  }
+
+  var scrollSpeed = 0;
+  function scrollAnimate(){
+    if(scrollPos < scrollFinish){
+
+      if(scrollPos >= scrollFinish - easeLength){
+        scrollSpeed = scrollSpeed > 1 ? scrollSpeed - easeSpeed : 1;
+      }else{
+        scrollSpeed = scrollSpeed < maxSpeed ? scrollSpeed + easeSpeed : maxSpeed;
+      }
+      scrollPos = scrollSpeed + scrollPos;
+      window.scrollTo(0, scrollPos);
+      window.requestAnimationFrame(scrollAnimate);
+    }else{
+      scrollSpeed = 0;
+    }
+  }
 
 </script>
 
